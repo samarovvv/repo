e8e148f40b0c4a04f5bd3dd09aa0c8b4afee306d
samarovvv <martin_samarov@hotmail.com>
2024-09-26 16:35:43 +0200

    Initial commit

--- frontend/vite-project/src/FileUpload.jsx
+++ frontend/vite-project/src/FileUpload.jsx
@@ -8,7 +8,7 @@ const FileUpload = () => {
   useEffect(() => {
     const fetchImages = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/api/photos");
+        const response = await axios.get("https://repo-vb52.onrender.com/api/photos");
         setImages(response.data);
       } catch (error) {
         console.error("Error fetching images:", error);
@@ -36,13 +36,13 @@ const FileUpload = () => {
     }
 
     try {
-      await axios.post("http://localhost:3000/api/photos/upload", formData, {
+      await axios.post("https://repo-vb52.onrender.com/api/photos/upload", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
       // Optionally fetch images again to see the newly uploaded images
-      const response = await axios.get("http://localhost:3000/api/photos");
+      const response = await axios.get("https://repo-vb52.onrender.com/api/photos");
       setImages(response.data);
     } catch (error) {
       console.error("Error uploading files:", error);
